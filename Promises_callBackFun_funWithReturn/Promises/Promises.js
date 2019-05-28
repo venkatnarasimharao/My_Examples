64670f17e9f58db700a1489710d3a55f0bbec31f
venkatnarasimharao <38693729+venkatnarasimharao@users.noreply.github.com>
2019-05-28 17:05:02 +0530

    pop

--- Promises_callBackFun_funWithReturn/Promises/Promises.js
+++ Promises_callBackFun_funWithReturn/Promises/Promises.js
@@ -1,10 +1,3 @@
-// function check(n,CallBackfnAsAnArgument){
-//     if(n%2==0){
-//         CallBackfnAsAnArgument('even');
-//     }else{
-//         CallBackfnAsAnArgument('odd');
-//     }
-// };
 
 function f1(n){
     return new Promise (function(resolve,reject){
